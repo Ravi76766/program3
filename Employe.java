f30f1dcf30bcc842a36e7d3bfc77d896f1de2a5e
Ravi76766 <raviu2689@gmail.com>
2025-05-25 22:28:24 +0530

    save file

---
+++ Employe.java
@@ -0,0 +1,30 @@
+package program3;
+public class Employe {
+    private String name;
+    private int id;
+    private double salary;
+    private String address;
+
+    // Constructor
+    public Employe(String name, int id, double salary, String address) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        this.address = address;
+    }
+
+    // Method to display employee details
+    public void display() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Salary: " + salary);
+        System.out.println("Address: " + address);
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        Employe emp = new Employe("John Doe", 12345, 50000.0, "Hubbali");
+        emp.display();
+    }
+}
+
